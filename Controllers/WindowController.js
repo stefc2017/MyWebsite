3884f78ebe264135e5b45083c73ec67c74ef9848
Stefan <stefc2013@gmail.com>
2017-09-28 21:19:30 -0500

    * Added templates  * Finished navbar  * Testing

--- Controllers/WindowController.js
+++ Controllers/WindowController.js
@@ -1,7 +1,7 @@
 //use for localhost
-//const defaultUrl = "file:///C:/Users/Stefan/Desktop/Programming/web/MyWebsite/";
+const defaultUrl = "file:///C:/Users/Stefan/Desktop/Programming/web/MyWebsite/";
 //use for github pages
-const defaultUrl = "https://stefc2017.github.io/MyWebsite/";
+//const defaultUrl = "https://stefc2017.github.io/MyWebsite/";
 
     function changeUrl(page){
         window.location.href = defaultUrl + page;
