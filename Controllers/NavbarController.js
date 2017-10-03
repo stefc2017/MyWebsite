e3541adcec4ab94898a455b24be9876bc4f3001d
Stefan <stefc2013@gmail.com>
2017-10-02 22:20:27 -0500

    * Worked on technical skills page

---
+++ Controllers/NavbarController.js
@@ -0,0 +1,20 @@
+// var activeTab = '#homeTab';
+//
+// function setActiveTab(){
+//  var currentUrl = window.location.href;
+//
+//  if(currentUrl.indexOf("highschool.html")){
+//      activeTab = "#highSchoolTab";
+//  }
+//  else if(currentUrl.indexOf("#postsecondary.html")){
+//  }
+// }
+//
+// function removeActiveTab(){
+//     $(activeTab).removeClass("active");
+// }
+
+// function updateTabs(){
+//     removeActiveTab();
+//     setActiveTab();
+// }
---
+++ Controllers/WindowController.js
@@ -0,0 +1,8 @@
+//use for localhost
+//const defaultUrl = "file:///C:/Users/Stefan/Desktop/Programming/web/MyWebsite/";
+//use for github pages
+const defaultUrl = "https://stefc2017.github.io/MyWebsite/";
+
+function changeUrl(page){
+    window.location.href = defaultUrl + page;
+}
