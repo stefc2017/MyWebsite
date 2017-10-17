2db68c4976c8ff6089db9222edabd13dc7f9e484
Stefan <stefc2013@gmail.com>
2017-10-16 22:26:07 -0500

    * Changed to github url  * Added methods to start transferring over to non-manual conversion between  localhost and github

--- js/WindowController.js
+++ js/WindowController.js
@@ -1,8 +1,3 @@
-//use for localhost
-const defaultUrl = "file:///C:/Users/Stefan/Desktop/Programming/web/MyWebsite/";
-//use for github pages
-//const defaultUrl = "https://stefc2017.github.io/MyWebsite/";
-
 var initialHeight;
 
     function setClientWindowHeight(backgroundHeight){
@@ -10,7 +5,8 @@ var initialHeight;
     }
 
     function changeUrl(page){
-        window.location.href = defaultUrl + page;
+        alert(getUrl());
+        window.location.href = getUrl() + page;
     }
 
     function animateBackground(){
