a68a8b78ff70e279e747530b6ba2d6c949965fa8
Stefan <stefc2013@gmail.com>
2017-10-17 20:28:41 -0500

    * testing github not working on github

--- js/MainController.js
+++ js/MainController.js
@@ -11,9 +11,11 @@ function initialize(){
 function setUrl(){
     if(getEnvironment() === "dev"){
         currentUrl = "https://stefc2017.github.io/MyWebsite/";
+        alert("dev");
     }
-    else{
+    else if(getEnvironment() === "localhost"){
         currentUrl = "file:///C:/Users/Stefan/Desktop/Programming/web/MyWebsite/";
+        alert("localhost");
     }
 }
 
@@ -22,31 +24,41 @@ function getUrl(){
 
     if(getEnvironment() === "dev"){
         url = "https://stefc2017.github.io/MyWebsite/";
+        alert("dev");
     }
-    else{
+    else if(getEnvironment() === "localhost"){
         url= "file:///C:/Users/Stefan/Desktop/Programming/web/MyWebsite/";
+        alert("localhost");
     }
     return url;
 }
 
 function setEnvironment(env){
+    alert(env + " hola");
     environment = env;
 }
 
 function getEnvironment(){
+    alert(environment + " uno dos");
     return environment;
 }
 
 function setAppEnvironment(){
     //on github
     if(location.hostname === "stefc2017.github.io"){
+        alert("dev" + " 29");
         setEnvironment("dev");
     }
     else{ //localhost
         setEnvironment("localhost");
+        alert("local");
     }
 }
 
 function changeUrl(page){
+    alert(window.location.href + " pug");
+    alert(getUrl() + " get url");
+    alert(page + " page");
     window.location.href = getUrl() + page;
+    alert(window.location.href + " pug2");
 }
